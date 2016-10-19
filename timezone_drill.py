8b93757389a15aebad3225cd07afb7bdb92a29c4
Schola Choi <scholachoi@gmail.com>
2016-10-18 17:34:42 -0700

    Update timezone_drill.py

--- timezone_drill.py
+++ timezone_drill.py
@@ -1,3 +1,21 @@
+"""
+Title: Datetime – Python 2.7 – IDLE
+Scenario: The company you work for just opened two new branches. One is in New York City,
+the other in London. They need a very simple program to find out if the branches are open or
+closed based on the current time of the Headquarters here in Portland. The hours of both
+branches are 9:00AM - 9:00PM in their own time zone.
+What is asked of you:
+Create code that will use the current time of the Portland HQ to find out the time in the NYC &
+London branches, then compare that time with the branches hours to see if they are open or
+closed.
+Print out if each of the two branches are open or closed.
+Guidelines:
+● Use Python 2.7 IDLE
+● Use Datetime Module
+● Execute program on the Shell
+"""
+
+
 from datetime import datetime, time
 from pytz import timezone
 
