97ab42962eb42862cbdaccfdc099be885594ddb7
Schola Choi <scholachoi@users.noreply.github.com>
2017-08-30 10:47:26 -0700

    Update file name and comment

--- file_mover.py
+++ file_mover.py
@@ -1,16 +1,12 @@
 """
-Title:File Mover Drill
-Scenario: Your employer wants a program to move all his .txt files from one
-folder to another with the click of a button. On your desktop make 2 new folders:
-Folder A and Folder B. Create 4 random .txt files and put them in Folder A.
-Plan:
+Title:File Mover
+Scenario: An employer wants a program to move all .txt files from one
+folder to another with the click of a button. 
+What the program does:
 -Move the files from Folder A to folder B
 -Print out each file path that got moved onto the shell
--After execution, the moved files should not be in Folder A
-Guidelines:
+Requirements:
 -Use Python 2.7x
--Import shutil module
--Run it on the Python shell
 -Use IDLE
 """
 
